70024475001a4e9d541542eb8263303053cc29d9
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-01-21 09:18:54 +0700

    creat class Contact

---
+++ src/model/Contact.java
@@ -0,0 +1,93 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Contact {
+    private int numberPhone;
+    private String group;
+    private String fullName;
+    private String gender;
+    private String address;
+    private LocalDate birthday;
+    private String email;
+    public Contact(){};
+
+    public Contact(int numberPhone, String group, String fullName, String gender, String address, LocalDate birthday, String email) {
+        this.numberPhone = numberPhone;
+        this.group = group;
+        this.fullName = fullName;
+        this.gender = gender;
+        this.address = address;
+        this.birthday = birthday;
+        this.email = email;
+    }
+
+    public int getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(int numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "numberPhone=" + numberPhone +
+                ", group='" + group + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", birthday=" + birthday +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
